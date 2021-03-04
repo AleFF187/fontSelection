ddd33feaaec2f9d1544fbd6f4e03e1df14e87d2a
AleFF187 <1871847@gmail.com>
2021-03-04 17:54:06 +0200

    некоторый эксперимент по структуре, хочу сохранить

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// let requestURL = '../fonts/fonts.json';
+// let request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+// request.responseType = 'json';
+// request.send();
+// request.onload = function() {
+//     var superHeroes = request.response; //     
+//     console.log(superHeroes);
+// }
+
+let fontsList = {
+    "font_type_001": "Mason Chronicles",
+    "font_fileName_001": "001_Mason_Chronicles.ttf",
+    "font_type_002": "Mason Chronicles 2",
+    "font_fileName_002": "002_Mason_Chronicles2.ttf"
+}
+
+console.log(fontsList);
