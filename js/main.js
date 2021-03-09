fec8b823ea8fe31e6937a23748544f8c9c984d77
AleFF187 <1871847@gmail.com>
2021-03-09 10:43:21 +0200

    вариант с кнопками "скрыть" "показать" шрифт

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-//
+let fontCards = document.querySelectorAll('.font-card');
+let cardResize = document.querySelectorAll('.font-card__resize');
+
+for (let i = 0; i < cardResize.length; i++) {
+    cardResize[i].addEventListener('click', function() {
+        if (fontCards[i].classList.contains('minimize')) {
+            fontCards[i].classList.remove('minimize');
+            cardResize[i].innerHTML = 'Скрыть';
+        } else {
+            fontCards[i].classList.add('minimize');
+            cardResize[i].innerHTML = 'Показать';
+        }
+    })
+}
