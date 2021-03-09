2cf98bddffa7218dc1cbdb58752108ae471dc40f
AleFF187 <1871847@gmail.com>
2021-03-09 12:21:08 +0200

    переделал логику (копка скрыть убирает шрифт, возврат через спсиок шрифтов)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 let fontCards = document.querySelectorAll('.font-card');
-let cardResize = document.querySelectorAll('.font-card__resize');
+let minimizeFontBtn = document.querySelectorAll('.font-card__minimize-button');
+let maximizeFontBtn = document.querySelectorAll('.font-number__maximize-button');
 
-for (let i = 0; i < cardResize.length; i++) {
-    cardResize[i].addEventListener('click', function() {
-        if (fontCards[i].classList.contains('minimize')) {
-            fontCards[i].classList.remove('minimize');
-            cardResize[i].innerHTML = 'Скрыть';
-        } else {
-            fontCards[i].classList.add('minimize');
-            cardResize[i].innerHTML = 'Показать';
-        }
+
+for (let i = 0; i < minimizeFontBtn.length; i++) {
+    minimizeFontBtn[i].addEventListener('click', function() {
+        fontCards[i].classList.add('minimize');
+    })
+}
+
+for (let i = 0; i < maximizeFontBtn.length; i++) {
+    maximizeFontBtn[i].addEventListener('click', function() {
+        fontCards[i].classList.remove('minimize');
     })
 }
