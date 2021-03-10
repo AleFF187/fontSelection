931bfe5e046adf75b95a55eb72a3c0e282de906e
AleFF187 <1871847@gmail.com>
2021-03-10 17:11:54 +0200

    рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
+//! объект со списком шрифтов
+fontsList = {
+    fontType001: '001_Mason_Chronicles.ttf',
+    fontType002: '002_Astoria_Script_Two.ttf'
+}
+
+// console.log(fontsList);
+
 let fontCards = document.querySelectorAll('.font-card');
 let minimizeFontBtn = document.querySelectorAll('.font-card__minimize-btn');
 let maximizeFontBtn = document.querySelectorAll('.maximize-btn');
 let maximizeAllFontBtn = document.querySelector('.maximize_all-btn');
-// console.log(maximizeAllFontBtn);
 
 
+// обработка нажатия кнопки "скрыть" на образце шрифта
 for (let i = 0; i < minimizeFontBtn.length; i++) {
     minimizeFontBtn[i].addEventListener('click', function() {
         fontCards[i].classList.add('minimize');
     })
 }
 
+// обработка нажатия кнопки с номером шрифта в списке скрытых и возврат его на экран
 for (let i = 0; i < maximizeFontBtn.length; i++) {
     maximizeFontBtn[i].addEventListener('click', function() {
         fontCards[i].classList.remove('minimize');
     })
 }
 
-
+// обработка нажатия кнопки "показать все" и возврат всех шрифтов  на экран
 maximizeAllFontBtn.addEventListener('click', function() {
     for (let i = 0; i < fontCards.length; i++) {
         fontCards[i].classList.remove('minimize');
     }
 })
-
-// maximizeAllFontBtn.onclick = function() {
