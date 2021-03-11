f5a4e53485b2167115f238d10a953aaf154b4ce6
AleFF187 <1871847@gmail.com>
2021-03-11 15:48:56 +0200

    решил переделать. смена классов,

--- js/main.js
+++ js/main.js
@@ -1,49 +1,59 @@
 //! объект со списком шрифтов
 fontsList = {
-        fontType001: '001_Mason_Chronicles.ttf',
-        fontType002: '002_Astoria_Script_Two.ttf'
-    }
-    //! переделать
+    fontType001: '001_Mason_Chronicles.ttf',
+    fontType002: '002_Astoria_Script_Two.ttf'
+}
+
+//! переделать
 let fontsListKeys = Object.keys(fontsList);
 
 
-let fontCards = document.querySelectorAll('.font-card');
-let minimizedFonts = document.querySelector('.minimized_fonts');
-let minimizeFontBtn = document.querySelectorAll('.font-card__minimize-btn');
-let maximizeFontBtn = document.querySelectorAll('.maximize-btn');
-let maximizeAllFontBtn = document.querySelector('.maximize_all-btn');
-
-// console.log(fontsListKeys);
 
 
-// обработка нажатия кнопки "скрыть" на образце шрифта
-for (let i = 0; i < minimizeFontBtn.length; i++) {
-    minimizeFontBtn[i].addEventListener('click', function() {
-        fontCards[i].classList.add('minimize');
-        let newBtn = document.createElement('button');
-        // let newBtn = document.createElement('div');
-        // newBtn.innerHTML = '<button class="maximize-btn btn">001</button> '
-        newBtn.classList.add('maximize-btn');
-        newBtn.classList.add('btn');
-        //! переделать
-        newBtn.innerHTML = fontsListKeys[i];
-        minimizedFonts.appendChild(newBtn);
+// let fontCards = document.querySelectorAll('.font-card');
+// let minimizedFontsBtns = document.querySelector('.minimized_fonts__buttons');
+// let minimizeFontBtn = document.querySelectorAll('.font-card__minimize-btn');
+// let maximizeFontBtns;
+// // let maximizeFontBtn = document.querySelectorAll('.maximize-btn');
+// let maximizeAllFontBtn = document.querySelector('.maximize_all-btn');
 
-        //! еще надо как то ослеживать добавленные кнопки
-        //! может быть надо сделать под них массив или же искать их на лету?
-    })
-}
+// // console.log(fontsListKeys);
 
-// обработка нажатия кнопки с номером шрифта в списке скрытых и возврат его на экран
-for (let i = 0; i < maximizeFontBtn.length; i++) {
-    maximizeFontBtn[i].addEventListener('click', function() {
-        fontCards[i].classList.remove('minimize');
-    })
-}
 
-// обработка нажатия кнопки "показать все" и возврат всех шрифтов  на экран
-maximizeAllFontBtn.addEventListener('click', function() {
-    for (let i = 0; i < fontCards.length; i++) {
-        fontCards[i].classList.remove('minimize');
-    }
-})
+// // обработка нажатия кнопки "скрыть" на образце шрифта
+// for (let i = 0; i < minimizeFontBtn.length; i++) {
+//     minimizeFontBtn[i].addEventListener('click', function() {
+//         fontCards[i].classList.add('minimize');
+//         let newBtn = document.createElement('button');
+//         //! newBtn.innerHTML = '<button class="maximize-btn btn">001</button> '
+//         newBtn.classList.add('maximize-btn');
+//         newBtn.classList.add('btn');
+//         newBtn.innerHTML = fontsListKeys[i];
+//         minimizedFontsBtns.appendChild(newBtn);
+//         maximizeFontButtonsEvent();
+//     })
+// }
+
+// function getMaximizeFontButtonsList() {
+//     // создаем список кнопок возврата шрифта в видимые
+//     maximizeFontBtns = document.querySelectorAll('.maximize-btn');
+// }
+
+
+// function maximizeFontButtonsEvent() {
+//     // добавляем кнопкам возврата (шрифта в видимые шрифты) обработку нажатия
+//     getMaximizeFontButtonsList();
+//     for (let i = 0; i < maximizeFontBtns.length; i++) {
+//         maximizeFontBtns[i].addEventListener('click', function() {
+//             fontCards[i].classList.remove('minimize');
+//         })
+//     }
+//     console.log(maximizeFontBtns);
+// }
+
+// // обработка нажатия кнопки "показать все" и возврат всех шрифтов  на экран
+// maximizeAllFontBtn.addEventListener('click', function() {
+//     for (let i = 0; i < fontCards.length; i++) {
+//         fontCards[i].classList.remove('minimize');
+//     }
+// })
